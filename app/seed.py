7370b6fb3048ff641789799748c732cafc4d8397
philipmuhoro <tasniim@DESKTOP-5THH7BR>
2024-02-20 13:14:56 +0300

    initial

--- app/seed.py
+++ app/seed.py
@@ -1,4 +1,5 @@
-from random import choice, choices, randint
+# from random import choice, choices, sample, randint
+import random
 import faker
 
 from models import Restaurant,Restaurant_pizza,Pizza, db
@@ -8,41 +9,48 @@ fake = faker.Faker()
 from app  import app
 
 with app.app_context():
-    
+    print("++++++++++++++++++++++++++++++++++++++")
+    print("Deleting data...")
     Pizza.query.delete()
     Restaurant_pizza.query.delete()
     Restaurant.query.delete()
 
-    for n in range(30):
+    print("++++++++++++++++++++++++++++++++++++")
+    print("Creating data...")
+    for n in range(20):
         fake_name=  fake.name()
         address= fake.address()
 
-        
+        # Create a restaurant
         restaurant1 = Restaurant(name=fake_name ,address=address)
+        # Add the restaurant to database
         db.session.add(restaurant1)
         db.session.commit()
 
-    
-    pizzas= ["Cheese Pizza", "Supreme Pizza", "Meat Pizza" , "Margherita Pizza", "BBQ Chicken Pizza", "Veggie Pizza", "Pepperoni Pizza"]
-    sample_ingredients= ["pepperoni" , "mushroom" ,"mozzarella", "onion", "bacon", "oregano", "olives",  "anchovies",  "ham"]
-    
+    #random.sample(ingredients, k=3) to select 3 random ingredients instead of potentially selecting the same pizza twice.
 
-    random_pizzas= []
+    pizzas= ["Peperoni", "Chicken tika", "Mushroom" , "Hawaiian", "Beef", "Barbeque Chicken"]
+    sample_ingredients= ["Pepperoni","Onion","Mushroom","Green Pepper","Red Pepper","Olives","Bacon","Chicken","Vegetables"]
+    # other_pizza = choices(sample_ingredients , k=3)
+    # random_ingedients= ','.join(str(ing) for ing in other_pizza)
 
-    for n in range(len(pizzas)):
-        other_pizza = choices(sample_ingredients , k=3)
-        random_ingedients= ','.join(str(ingredient) for ingredient in other_pizza)
-        random_pizza= Pizza(name= choice(pizzas), ingredients= random_ingedients)
-        random_pizzas.append(random_pizza)
+    fake_pizzas= []
 
-    print(random_pizzas)
-    db.session.add_all(random_pizzas)
+    for x in range(len(pizzas)):
+        other_pizza = random.choices(sample_ingredients , k=3)
+        random_ingedients= ','.join(str(ing) for ing in other_pizza)
+        fake_pizza= Pizza(name= random.choice(pizzas), ingredients= random_ingedients)
+        fake_pizzas.append(fake_pizza)
+
+    print(fake_pizzas)
+    db.session.add_all(fake_pizzas)
     db.session.commit()
 
-    for record in range(15):
-        rnd_rest=choice([x.id for x in Restaurant.query.all()])
-        rnd_pizza= choice([p.id for p in  Pizza.query.all()])
-        db.session.add(Restaurant_pizza(restaurant_id=rnd_rest, pizza_id=rnd_pizza, price= randint(1,30)))
+    for record in range(20):
+        rnd_rest=random.choice([x.id for x in Restaurant.query.all()])
+        rnd_pizza= random.choice([p.id for p in  Pizza.query.all()])
+        db.session.add(Restaurant_pizza(restaurant_id=rnd_rest, pizza_id=rnd_pizza, price= random.randint(1,30)))
         db.session.commit()
 
-    
+    print("++++++++++++++++++++++++++++++++++++")
+    print("Finished creating data...")
